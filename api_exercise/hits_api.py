9546c0083c9c650ddf65c9c40ed16673a03ecf62
Andres Garcia <aagarciat@yahoo.com>
2019-05-15 15:01:21 -0500

    First fixes and improvements

--- api_exercise/hits_api.py
+++ api_exercise/hits_api.py
@@ -1,5 +1,10 @@
+# Your last JavaScript (Node) code is saved below:
+# //Desing and implement 2 functions
+# //1. logHit => no args, return null
+# //2. getHitlast5Min => no args, return number
+# //teechip.com
 import time
-from datetime import date, timedelta
+import datetime
 
 
 def logHit(id, time):
@@ -12,42 +17,34 @@ def logHit(id, time):
 
 
 def getHitLast5Min():
-  now = date.today()
+  global userVisits
+  now = datetime.datetime.now()
   count = 0
 
-  for user in userVisits:
-    for time in user:
-      if now - time <= 5*60*1000:
+  for user, times in userVisits.items():
+    for time in times:
+      if ((now - time).total_seconds() / 60) <= 5:
         count += 1
 
   return count
 
 
 
-def testVisits():
+def testVisits(num_users=1000, sleep_sec=1):
   global userVisits
 
-  for i in range(100):
-    #time.sleep(100)
-    logHit('user'+str(i), date.today())
-
+  for i in range(num_users):
+    time.sleep(sleep_sec) # one user per second, i.e. 60 users per minute
+    logHit('user_'+str(i), datetime.datetime.now())
+    
   n = getHitLast5Min()
 
-  if n != 1000:
+  if n != 5*60: # 60*5 users in five minutes expected.
     print(n)
-    return "Failure"
+    print("Failure")
 
-  return "Success"
+  print("Success")
 
 
 userVisits = {}
 testVisits()
-print("Hello")
-
-
-
-# Your last JavaScript (Node) code is saved below:
-# //Desing and implement 2 functions
-# //1. logHit => no args, return null
-# //2. getHitlast5Min => no args, return number
-# //teechip.com
